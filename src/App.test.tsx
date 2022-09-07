3b1c1a4a84849af86de003bd70864ec54c01f9d6
Andrey Babkin <babkin.andrey@gmail.com>
2022-09-07 19:15:11 +0300

    fixed test error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,13 @@
 import { render, screen } from './test-utils/testing-utils';
+import {act, waitFor} from '@testing-library/react'
 import App from './App';
 
 
-test('renders heading', () => {
+test('renders heading', async () => {
+  
   render(
       <App />
   );
-  const header = screen.getByRole('heading', { name: 'Readest' });
+  const header = await screen.findByRole('heading', { name: 'Readest' });
   expect(header).toBeInTheDocument();
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,11 +3,25 @@ import '@testing-library/jest-dom'
 // src/setupTests.js
 import { server } from './mocks/server.js'
 // Establish API mocking before all tests.
-beforeAll(() => server.listen())
+beforeAll(() => server.listen());
+
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
 
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
 afterEach(() => server.resetHandlers())
 
 // Clean up after the tests are finished.
-afterAll(() => server.close())
+afterAll(() => server.close());
+
+afterAll(() => {
+  console.error = originalError
+})
