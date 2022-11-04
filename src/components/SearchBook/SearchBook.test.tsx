36b7a1fc42f5917f403f19d2396bcc7fe818e192
Andrey Babkin <babkin.andrey@gmail.com>
2022-11-04 16:19:23 +0300

    added tests

--- src/components/SearchBook/SearchBook.test.tsx
+++ src/components/SearchBook/SearchBook.test.tsx
@@ -2,6 +2,7 @@ import { fireEvent, render, screen } from '../../test-utils/testing-utils';
 import userEvent from '@testing-library/user-event';
 import App from '../../App';
 
+
 const renderSearch = () => {
   render(<App />);
   const menuEl = screen.getByRole('link', {
@@ -30,18 +31,43 @@ describe('Search book functionality', () => {
 
     expect(searchTypeSelect).toBeInTheDocument();
   });
-	
-  test('default search by author is working', () => {
+
+  test('default search by author is working', async () => {
     renderSearch();
     const titleInput = screen.getByRole('textbox', { name: 'search-input' });
     userEvent.type(titleInput, 'City of Blades');
     const searchBtn = screen.getByRole('button', { name: 'search' });
-		expect(searchBtn).toBeInTheDocument();
-		fireEvent.click(searchBtn);
-		const headingSearchResult = screen.getByRole('heading', {
+    expect(searchBtn).toBeInTheDocument();
+    fireEvent.click(searchBtn);
+    const headingSearchResult = screen.getByRole('heading', {
       name: 'Your search results',
       level: 3,
     });
-		expect(headingSearchResult).toBeInTheDocument();
+    expect(headingSearchResult).toBeInTheDocument();
+    const spinner = screen.getByTestId('spinner');
+    expect(spinner).toBeInTheDocument();
+    // const galleryItems = await screen.findAllByAltText(
+    //   /cover$/,
+    //   {},
+    //   { timeout: 5000 }
+    // );
+    // expect(galleryItems.length).toBeGreaterThan(0);
+		// const sampleBook = screen.getByRole('link', {name: 'City of Blades: The Divine Cities Book 2 cover'});
+		// userEvent.click(sampleBook);
+		// eslint-disable-next-line testing-library/no-debugging-utils
+		// screen.debug();
+		// // const menuElNew = screen.getByRole('link', {
+		// // 	name: 'Add Book',
+		// // });
+		// // userEvent.click(menuElNew);
+
+		// const headingForm =  screen.getByRole('heading', {
+    //   name: 'My books',
+    //   level: 2,
+    // });
+    // expect(headingForm).toBeInTheDocument();
+
+		
+		
   });
 });
--- src/components/SearchBook/SearchItem/SearchItem.tsx
+++ src/components/SearchBook/SearchItem/SearchItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { StyledSearchItem } from './SearchItem.styles';
+import { Link } from 'react-router-dom';
 import emptyCover from '../../../assets/images/emptyCover_sm.png';
 
 type SearchItemProps = {
@@ -17,34 +17,33 @@ const SearchItem: React.FC<SearchItemProps> = ({
   coverId,
   isbn,
 }) => {
-  let navigate = useNavigate();
-  const onItemClick = () => {
-    navigate(
-      `/add/?title=${encodeURIComponent(title)}&author=${encodeURIComponent(
-        author
-      )}&isbn=${encodeURIComponent(
-        isbn
-      )}`
-    );
-  };
+
   return (
     <StyledSearchItem className="search-item">
       <div className="search-item__wrapper">
         <div className="search-item__image">
-          <img
-            src={
-              coverId === 0
-                ? emptyCover
-                : `https://covers.openlibrary.org/b/id/${coverId}-M.jpg?default=false`
-            }
-            onError={({ currentTarget }) => {
-              currentTarget.onerror = null; // prevents looping
-              currentTarget.src = emptyCover;
-            }}
-            onClick={onItemClick}
-            alt={`${title} cover`}
-            loading="lazy"
-          />
+          <Link
+            to={`/add/?title=${encodeURIComponent(
+              title
+            )}&author=${encodeURIComponent(author)}&isbn=${encodeURIComponent(
+              isbn
+            )}`}
+          >
+            <img
+              src={
+                coverId === 0
+                  ? emptyCover
+                  : `https://covers.openlibrary.org/b/id/${coverId}-M.jpg?default=false`
+              }
+              onError={({ currentTarget }) => {
+                currentTarget.onerror = null; // prevents looping
+                currentTarget.src = emptyCover;
+              }}
+              // onClick={onItemClick}
+              alt={`${title} cover`}
+              loading="lazy"
+            />
+          </Link>
         </div>
         <div className="search-item__title">
           <span>
